11beb2ad34ccbab91dc9435db0d0a3ca35d86945
Daniel Hsieh <a761007@gmail.com>
2015-12-24 12:13:43 +0800

    Add postfix for mapping @3x image.

--- img_resizer/resizer.py
+++ img_resizer/resizer.py
@@ -8,11 +8,12 @@ curdir = os.path.abspath('.')
 @click.command()
 @click.option('--path', default=curdir, help='the directory of images (default: ".")')
 @click.option('--format', default='png', help='image format (default: png)')
-def resize(path, format):
+@click.option('--postfix', default='@3x.png', help='mapping @3x image (default: @3x.png)')
+def resize(path, format, postfix):
     click.echo('current location = %s' % path)
     image_files = None
     for _, _, filenames in os.walk(path):
-        image_files = [filename for filename in filenames if filename.endswith('@3x.{0}'.format(format))]
+        image_files = [filename for filename in filenames if filename.endswith('{0}'.format(postfix))]
 
     success = []
     failure = []
@@ -20,13 +21,15 @@ def resize(path, format):
         click.echo('process image: %s' % filename)
         try:
             with Image.open(os.path.join(path, filename)) as im:
+                name = filename[:-len(postfix)]
+                im.save(os.path.join(path, '{0}@3x.{1}'.format(name, format)), format=format)
                 w, h = im.size
                 im2x = im.copy()
                 im2x.thumbnail((float(w) / 3 * 2, float(h) / 3 * 2))
-                im2x.save(os.path.join(path, filename).replace('@3x', '@2x'))
+                im2x.save(os.path.join(path, '{0}@2x.{1}'.format(name, format)), format=format)
                 im1x = im.copy()
                 im1x.thumbnail((float(w) / 3, float(h) / 3))
-                im1x.save(os.path.join(path, filename).replace('@3x', ''))
+                im1x.save(os.path.join(path, '{0}.{1}'.format(name, format)), format=format)
                 success.append(filename)
         except Exception as e:
             click.echo('error=%s' % e)
